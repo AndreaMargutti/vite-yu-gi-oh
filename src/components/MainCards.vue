5c9257f43de4f73ea5327b843603452fbf669000
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-18 17:55:08 +0200

    esercizio finito (no bonus)

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -11,21 +11,27 @@ export default {
       type: Object,
       required: true
     }
-  }
+  },
 }
 </script>
 
 <template>
   <div class="card w-100">
-  <img :src="cardObj.card_images.image_url" class="card-img-top" alt="...">
+  <img :src="cardObj.card_images[0].image_url" class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">
-      {{ cardObj.name }}
-      {{ cardObj.archetype }}
+      <span>{{ cardObj.name }}</span> <br>
+      <span>{{ cardObj.archetype }}</span>
     </p>
   </div>
 </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .card-body {
+    background-color: #d38e3a;
+    span:first-child {
+      color: white;
+    }
+  }
 </style>
