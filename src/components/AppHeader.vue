0916d7176bafc0d6d45242432d5345f07e3e0e4b
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-18 16:22:16 +0200

    completed AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,14 @@ export default {
 </script>
 
 <template>
-    <h1>Header</h1>
+  <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/1200px-Yu-Gi-Oh!_(Logo).jpg" alt="Logo" width="60" class="d-inline-block align-text-center">
+      <span class="fs-3">Yu-Ghi-Oh API</span>
+    </a>
+  </div>
+</nav>
 </template>
 
 <style scoped>
